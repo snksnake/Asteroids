833b3c72ade0f264296a5483fb8aa10f1ff24e1f
Alberto <snk_snake@hotmail.com>
2018-12-05 21:02:51 +0100

    Menu added

--- app/src/main/java/maa/asteroids/MainActivity.java
+++ app/src/main/java/maa/asteroids/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -55,5 +57,22 @@ public class MainActivity extends AppCompatActivity {
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
             Log.d("CONFIG", "Pantalla en Portrait");
         }
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true; /** true -> el menú ya está visible */
+    }
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.acercaDe) {
+            final Intent intent = new Intent(MainActivity.this, About.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
